dc4d930e9bfa7fe37995376609c50c0955662c1d
Gus Ireland <gus.ireland@rackspace.com>
2013-09-06 13:39:14 -0500

    Fixed view for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
  skip_before_filter :verify_authenticity_token  
-  def create
-   user = User.from_omniauth(env['omniauth.auth'])
-   session[:user_id] = user.id
-   redirect_to root_url, notice: "Signed in."
-  end
+ 
+ def create
+  user = User.from_omniauth(env['omniauth.auth'])
+  session[:user_id] = user.id
+  redirect_to 'games#show', notice: "Signed in."
+ end
 
 
 end
