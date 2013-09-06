768118b06c58eb16a3a2dbfb5a622b4bd082c0f5
Brendan Fosberry <brendan.fosberry@gmail.com>
2013-09-06 11:41:56 -0500

    Adding join between users and games

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+	has_and_belongs_to_many :games
+
 	create! do |user|
     user.provider = auth["provider"]
     user.uid = auth["uid"]
     user.name = auth["info"]["name"]
   end
+
+
+
 end
