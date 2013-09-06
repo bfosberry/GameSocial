ee58b38387234dd8df53caa279c775c0116d7f65
Brendan Fosberry <brendan.fosberry@gmail.com>
2013-09-06 13:11:50 -0500

    Updating game user link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      up = user_params
+      up[:games] = up[:game_ids].reject {|g|  g.blank? }.map {|id| Game.find(id) }
+      
+      if @user.update(up)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
@@ -69,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:provider, :uid, :name)
+      params.require(:user).permit(:provider, :uid, :name, :game_ids =>[])
     end
 end
