f50ccd91da64cd6890b64f6ccad52436f5ea6684
Brendan Fosberry <brendan.fosberry@gmail.com>
2013-09-06 12:15:39 -0500

    Removing create hook for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :games
-
-	create! do |user|
-    user.provider = auth["provider"]
-    user.uid = auth["uid"]
-    user.name = auth["info"]["name"]
-  end
 end
