d2fe85d232a3b641ea44ff679aa417ea73b502cc
Brendan Fosberry <brendan.fosberry@gmail.com>
2013-09-06 09:38:44 -0500

    Barebones rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameSocial::Application.config.secret_key_base = '01c2fcf30be73aa2250d7267fb2d907f1b9cfb47c44c0af798bdf884093f09c6ec1a5dafd7988fdd09f87b1827cc038714dec16299c8c1f318c51269554bd1ac'
