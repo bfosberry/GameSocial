072bdb34efbd1dd8a00061934eeb4ca92b2d9eb8
Brendan Fosberry <brendan.fosberry@rackspace.com>
2013-09-11 08:45:03 -0500

    Updating alert schedules edit view to use rendered views for each component. This makes the form data and rendering uniform across conditions

--- app/controllers/alert_schedules_controller.rb
+++ app/controllers/alert_schedules_controller.rb
@@ -69,8 +69,10 @@ class AlertSchedulesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def alert_schedule_params
-      params.require(:alert_schedule).permit!
-      #TODO fix this so we can pass in both cases for value
-      #(:user_id, :name,:alert_conditions_attributes => [:id, :value => [:start_time,  :end_time], :value => []])
+      params.require(:alert_schedule).permit(:user_id, :name, :alert_conditions_attributes => [
+        :id, 
+        :value => ['start_time(1i)', 'start_time(2i)', 'start_time(3i)', 'start_time(4i)', 
+                   'start_time(5i)', 'end_time(1i)', 'end_time(2i)', 'end_time(3i)', 'end_time(4i)', 
+                   'end_time(5i)', :games => [], :users => [], :days => []]])
     end
 end
