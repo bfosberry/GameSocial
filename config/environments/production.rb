7c733a4b65eacbd8f89a6ac031c4fcb7819b42c2
Brendan Fosberry <brendan.fosberry@gmail.com>
2013-09-29 01:16:54 -0500

    Setting smtp mailer settings based on ENV data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ GameSocial::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,14 @@ GameSocial::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
