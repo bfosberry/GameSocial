9c84c89b9bf71bf7f48dbc900dc66bcce83ff268
Brendan Fosberry <brendan.fosberry@rackspace.com>
2013-09-30 16:31:55 -0500

    Using custom branch for steam-condenser
    Adding basic requirements to track user location in steam

--- app/providers/steam_provider.rb
+++ app/providers/steam_provider.rb
@@ -2,12 +2,14 @@ module Providers
   class SteamProvider
     attr_accessor :steam_id, :user
     def initialize(user)
-      id = SteamId.community_id_to_steam_id(user.uid.to_i)
-      self.steam_id = SteamId.new(id)
+      SteamCondenser::Community::WebApi.api_key = ENV['STEAM_API_KEY']
+      id = SteamCondenser::Community::SteamId.community_id_to_steam_id(user.uid.to_i)
+      self.steam_id = SteamCondenser::Community::SteamId.new(id)
       self.user = user
       self.steam_id ? self : nil
-    rescue SteamCondenserError
-      nil
+#    rescue SteamCondenserError
+#
+#      nil
     end
 
     def games
