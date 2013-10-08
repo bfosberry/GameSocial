e186b3b95cc43b7b9ab7ddf098a63567965fd0db
Brendan Fosberry <brendan.fosberry@rackspace.com>
2013-10-08 14:32:48 -0500

    Adding admin field to user, adding admins to seed data
    Removing old seed data for games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008142157) do
+ActiveRecord::Schema.define(version: 20131008192612) do
 
   create_table "alert_conditions", force: true do |t|
     t.string   "condition_type"
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(version: 20131008142157) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.boolean  "admin"
   end
 
 end
