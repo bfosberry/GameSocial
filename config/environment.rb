302b6c4a8394257e8396340b468c546a62154abd
Brendan Fosberry <brendan.fosberry@rackspace.com>
2013-11-06 11:55:38 -0600

    Fixing bug with placement of SECRETS loading

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
-SECRETS = YAML.load_file("#{Rails.root}/config/secret.yml")[Rails.env]
 require File.expand_path('../application', __FILE__)
+SECRETS = YAML.load_file("#{Rails.root}/config/secret.yml")[Rails.env]
 
 # Initialize the Rails application.
 GameSocial::Application.initialize!
