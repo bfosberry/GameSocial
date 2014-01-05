d489050d0c693d510b8c149da133ef781747ed9f
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-01-04 19:20:31 -0600

    Fixing tabbing

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,9 +2,9 @@ class AlertMailer < ActionMailer::Base
   default from: "alerts@cloud1001.co"
 
   def alert_email(alert)
-  	@user = alert.user
-  	@body = alert.description
-  	@subject = alert.title
+    @user = alert.user
+    @body = alert.description
+    @subject = alert.title
     mail(to: @user.email, subject: @subject) if @user.email
   end
 end
