f1c4b95a2efdef6be24dc03a1e057ad0bc044543
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-01-04 19:21:18 -0600

    Adding debugging statements

--- lib/workers/location_sync_worker.rb
+++ lib/workers/location_sync_worker.rb
@@ -8,11 +8,14 @@ module Workers
 
     def perform(user_id)
       user = User.find(user_id)
+      puts "Syncing location for #{user.name}"
       sp = Providers::SteamProvider.new(user)
       si = Importers::SteamImporter.new(sp)
       si.import_location
+      puts "Completed sync location for #{user.name}"
     rescue SteamCondenser::Error => e
       puts "Failed to sync users location"
+      puts e
     end
   end
 end
