779d8baaa8383d04ccc5255e5e7d9abc3833840b
brendan.fosberry <brendan.fosberry@rackspace.com>
2014-01-16 17:49:11 -0600

    Adding news feed, fixing references to naming across models

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -3,6 +3,8 @@ class Alert < ActiveRecord::Base
   belongs_to :alert_schedule
   delegate :user, :to => :alert_schedule, :allow_nil => true
   delegate :name, :to => :alert_schedule, :prefix => true, :allow_nil => true
+  delegate :game_name, :to => :payload, :allow_nil => true
+  delegate :user_name, :to => :payload, :allow_nil => true
   serialize :payload
 
   after_create :deliver_alert
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+  delegate :name, :to => :friend, :prefix => true, :allow_nil => true
 end
