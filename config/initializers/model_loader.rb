0eae0863ffd0ef0b360378902bd90d1a1ab67c6e
brendan.fosberry <brendan.fosberry@rackspace.com>
2014-01-17 08:51:46 -0600

    Enforcing loaded models end in .rb

--- config/initializers/model_loader.rb
+++ config/initializers/model_loader.rb
@@ -1,5 +1,5 @@
 if Rails.env == "development"
   Dir.foreach("#{Rails.root}/app/models") do |model_name|
-    require_dependency model_name if model_name =~ /\w*\.rb/
+    require_dependency model_name if model_name =~ /\w*\.rb$/
   end 
 end
