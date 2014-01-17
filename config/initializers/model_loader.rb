6bb0133cfc319649ff21a20f497ddcdf66b39876
brendan.fosberry <brendan.fosberry@rackspace.com>
2014-01-17 08:45:05 -0600

    Removing puts statement

--- config/initializers/model_loader.rb
+++ config/initializers/model_loader.rb
@@ -1,6 +1,5 @@
 if Rails.env == "development"
   Dir.foreach("#{Rails.root}/app/models") do |model_name|
-    puts "loading #{model_name}"
     require_dependency model_name if model_name =~ /\w*\.rb/
   end 
 end
