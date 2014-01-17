3215c7fe88a616573e8dfd56c5c01145a02d2265
brendan.fosberry <brendan.fosberry@rackspace.com>
2014-01-17 08:52:02 -0600

    Applying sort to users alerts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   has_many :game_locations, :dependent => :destroy
-  has_many :alerts, :through => :alert_schedules
+  has_many :alerts, :through => :alert_schedules, :order => "created_at DESC"
 
   has_many :alerting_users, dependent: :destroy
   has_many :alerting_schedules, through: :alerting_users, :source => :alert_schedule
