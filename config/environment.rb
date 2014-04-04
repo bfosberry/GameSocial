1b3446e78afcdc86f14fc77597840115c39cae28
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-04-03 21:37:25 -0500

    Updating loaders to dynamically check for secrets file and existing config

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,18 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-SECRETS = YAML.load_file("#{Rails.root}/config/secret.yml")[Rails.env]
+unless SECRETS 
+  SECRETS = {}
+  SECRETS['steam_api_key'] = ENV['STEAM_API_KEY']
+  SECRETS['smtp_username'] = ENV['SMTP_USERNAME']
+  SECRETS['smtp_password'] = ENV['SMTP_PASSWORD']
+  SECRETS['smtp_server'] = ENV['SMTP_SERVER']
+  SECRETS['smtp_port'] = ENV['SMTP_PORT']
+
+  secret_file = "#{Rails.root}/config/secret.yml"
+  if File.exists?(secret_file)
+    SECRETS = YAML.load_file("#{Rails.root}/config/secret.yml")[Rails.env]
+  end
+end
 
 # Initialize the Rails application.
 GameSocial::Application.initialize!
