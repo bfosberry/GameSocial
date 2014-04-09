c3271795f1f240a794d7a77ad5bd288e9715e3b2
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-04-08 21:52:31 -0500

    Removing hardcode api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :steam, 'A67D7BFF425F5340678E8F8BD8BE114D'
+  provider :steam, SECRETS['steam_api_key']
 end
