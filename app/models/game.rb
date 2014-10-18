f76349e169ec6e5dfc0a5fc65aea4ded14e08a20
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-10-18 10:26:09 -0500

    Adding board game support to game event form Adding type filters for game views

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,29 @@ class Game < ActiveRecord::Base
 
   default_scope order('name ASC')
 
+  scope :board, -> { where(provider: "board")}
+  scope :steam, -> { where(provider: "steam")}
+
+  def self.type_fetchers
+  	Game.select("provider").order("provider").distinct.map {|g| GameTypeFetcher.new(g.provider)}
+  end
+
+  def self.types
+  	["steam", "board"]
+  end
+
+  class GameTypeFetcher
+    def initialize(type)
+      @type = type
+    end
+
+    def fetch
+      Game.where("provider = ?", @type)
+    end
+
+    def type
+      @type
+    end
+  end
 end
+
