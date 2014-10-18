682f247fe2a679d917aec933dc1758b93d067193
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-10-18 15:22:53 -0500

    Adding RecordNotFound to list if ignored Runtimeerror errors

--- config/initializers/runtimeerror_notifier.rb
+++ config/initializers/runtimeerror_notifier.rb
@@ -5,6 +5,7 @@ if defined?(RuntimeerrorNotifier)
   RuntimeerrorNotifier.for ENV['RUNTIMEERROR_EMAIL']
 
   RuntimeerrorNotifier::Notifier::IGNORED_EXCEPTIONS.push(*%w[
-    ActionController::RoutingError
+    ActionController::RoutingError 
+    ActiveRecord::RecordNotFound
   ])
 end
