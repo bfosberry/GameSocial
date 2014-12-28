0db84e7f2dfb144211381c1a559602adab4990a5
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-12-28 09:42:12 +0000

    Adding developer callback url, updating auth callbacks to hit credentials resource

--- config/routes.rb
+++ config/routes.rb
@@ -55,8 +55,10 @@ GameSocial::Application.routes.draw do
 
   resources :games
   root to: 'users#home'
-  match 'auth/steam/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/google_oauth2/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/steam/callback', to: 'credentials#create', via: [:get, :post]
+  match 'auth/google_oauth2/callback', to: 'credentials#create', via: [:get, :post]
+  match 'auth/developer/callback', to: 'credentials#create', via: [:get, :post]
+ 
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   mount Sidekiq::Web => '/sidekiq'
