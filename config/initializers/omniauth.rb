34d7db470b810adbb74849b6bcaa49e73b6b7170
Brendan Fosberry <brendan.fosberry@gmail.com>
2014-12-28 09:41:54 +0000

    Updating steam api key loader to use ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :steam, SECRETS['steam_api_key']
+  provider :steam, ENV['STEAM_API_KEY']
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
