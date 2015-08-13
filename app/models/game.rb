3274c1f0c5a1cdf58ef10da2e2fe9242f3e89750
Brendan Fosberry <brendan.fosberry@gmail.com>
2015-08-13 18:59:43 -0500

    fixing game.find_by_name bug

--- app/models/game.rb
+++ app/models/game.rb
@@ -23,7 +23,7 @@ class Game < ActiveRecord::Base
   end
 
   def self.find_by_name(game_name)
-    find_by(:name, sanitized_name(game_name))
+    find_by(name: sanitized_name(game_name))
   end
 
   def self.sanitized_name(game_name)
