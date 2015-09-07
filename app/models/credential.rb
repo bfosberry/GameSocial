5f045bb60cb047b0064e6b17e39c1a2f028d6c55
bfosberry <brendan@codeship.com>
2015-09-07 12:27:58 -0500

    Updating creds creation to suite strong params issues

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,24 +2,24 @@ class Credential < ActiveRecord::Base
   belongs_to :user
 
   def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |cred|
+      create_from_omniauth(auth, cred)
+    end
   end
 
-  def self.create_from_omniauth(auth)
-    create! do |cred|
-      cred.provider = auth["provider"]
-      cred.uid = auth["uid"]
-      cred.image_url = auth["info"]["image"]
-      cred.name = auth["info"]["name"]
-      if cred.is_google?
-        cred.email = auth["info"]["email"]
-        cred.profile_url = auth["extra"]["raw_info"]["profile"]
-        cred.refresh_token = auth["credentials"]["token"]
-        cred.token_expiry = auth["credentials"]["expires_at"]
-      elsif cred.is_steam?
-        cred.nickname = auth["info"]["nickname"]
-        cred.profile_url = auth["extra"]["raw_info"]["profileurl"]
-      end  
+  def self.create_from_omniauth(auth, cred)
+    cred.provider = auth["provider"]
+    cred.uid = auth["uid"]
+    cred.image_url = auth["info"]["image"]
+    cred.name = auth["info"]["name"]
+    if cred.is_google?
+      cred.email = auth["info"]["email"]
+      cred.profile_url = auth["extra"]["raw_info"]["profile"]
+      cred.refresh_token = auth["credentials"]["token"]
+      cred.token_expiry = auth["credentials"]["expires_at"]
+    elsif cred.is_steam?
+      cred.nickname = auth["info"]["nickname"]
+      cred.profile_url = auth["extra"]["raw_info"]["profileurl"]
     end
   end
 
