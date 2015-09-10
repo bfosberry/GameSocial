03891469f6df740c87f52228929d1abb8a706eff
Brendan Fosberry <brendan.fosberry@gmail.com>
2015-09-09 20:35:53 -0500

    Adding simple caching for steam_provider

--- app/providers/steam_provider.rb
+++ app/providers/steam_provider.rb
@@ -1,6 +1,11 @@
 module Providers
   class SteamProvider
     attr_accessor :steam_id, :user
+    GAMES_EXPIRY=12.hours
+    FRIENDS_EXPIRY=12.hours
+    NAME_EXPIRY=12.hours
+    AVATAR_URL_EXPIRY=12.hours
+    SUMMARY_EXPIRY=1.minutes
     def initialize(user)
       self.user = user
       SteamCondenser::Community::WebApi.api_key = ENV['STEAM_API_KEY']
@@ -9,31 +14,65 @@ module Providers
     end
 
     def games
-      steam_id.games
+      Rails.cache.fetch(game_cache_key, :expires_in => GAMES_EXPIRY) do
+        steam_id.games
+      end
     rescue
       {}
     end
 
     def friends
-      steam_id.friends
+      Rails.cache.fetch(friends_cache_key, :expires_in => FRIENDS_EXPIRY) do
+        steam_id.friends
+      end
     rescue 
       {}
     end
 
     def name
-      steam_id.nickname
+      Rails.cache.fetch(name_cache_key, :expires_in => NAME_EXPIRY) do
+        steam_id.nickname
+      end
     end
 
     def avatar_url
-      steam_id.full_avatar_url
+      Rails.cache.fetch(avatar_url_cache_key, :expires_in => AVATAR_URL_EXPIRY) do
+        steam_id.full_avatar_url
+      end
     end
 
     def current_game
-      steam_id.summary[:game_name] if steam_id.summary
+      summary[:game_name] if summary
     end
 
     def current_game_server
-      steam_id.summary[:game_server_ip] if steam_id.summary
+      summary[:game_server_ip] if summary
+    end
+
+    def summary
+      Rails.cache.fetch(summary_cache_key, :expires_in => SUMMARY_EXPIRY) do
+        steam_id.summary
+      end
+    end
+
+    def games_cache_key
+      "steam_#{user.id}_#{user.steam_uid}_games"
+    end
+
+    def friends_cache_key
+      "steam_#{user.id}_#{user.steam_uid}_friends"
+    end
+
+    def name_cache_key
+      "steam_#{user.id}_#{user.steam_uid}_name"
+    end
+
+    def avatar_url_cache_key
+      "steam_#{user.id}_#{user.steam_uid}_avatar_url"
+    end
+
+    def summary_cache_key
+      "steam_#{user.id}_#{user.steam_uid}_summary"
     end
   end
 end
