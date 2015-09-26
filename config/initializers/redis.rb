16a5a8a1707ee598b3eeb4efeb7bfd639b31b163
Brendan Fosberry <brendan.fosberry@gmail.com>
2015-09-26 12:52:09 -0500

    Updating cahe to ot use in-memory on heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
-REDIS = Redis.new(:url => uri, :namespace => "gamekick_#{Rails.env}")
+REDIS_URI = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+REDIS = Redis.new(:url => REDIS_URI, :namespace => "gamekick_#{Rails.env}")
